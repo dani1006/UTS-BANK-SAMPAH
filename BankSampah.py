e3c9f8347b1f39c7d077035a0a610d375641662c
dani1006 <73922058+dani1006@users.noreply.github.com>
2020-12-15 12:14:02 +0700

    Update BankSampah.py

--- BankSampah.py
+++ BankSampah.py
@@ -1,1 +1,33 @@
+# sistem bank sampah 
 
+nama = input("Siapa nama Anda: ")
+alamat = input("Alamat: ")
+
+pilihan = 1
+while pilihan !=0:
+    print()
+    print("---Menu Bank Sampah---")
+    print("1. Harga Sampah /KG")
+    print("2. Ingin Stor ")
+    print("0. exit")
+    print()
+    pilihan = int(input("Masukkan Nomor Menu: "))
+    if pilihan == 1:
+        print("Harga Untuk Anggota yang sudah lebih dari 5 bulan = 5000")
+        print("Harga Untuk Anggota baru/ kurang dari 5 bulan = 2500")
+    elif pilihan == 2:
+        harga()  
+    
+def harga():
+    berat_sampah = int(input("Berapa Kilo sampah anda? "))
+    lama_gabung = input("apakah anda sudah bergabung selama lebih dari 5 bulan?[Y/T]")
+    if lama_gabung == "y":
+        harga_sampah_tambahan = berat_sampah * 5000
+        print()
+        print("Harga Sampah: ",harga_sampah_tambahan)
+        print()
+    else:
+        harga_sampah = berat_sampah * 2500
+        print()
+        print("Harga sampah: ",harga_sampah)
+        print()
